22b6d08a710fd9ca092a2a44474b9433e0ca6ea2
fo40225 <fo40225@hotmail.com>
2016-03-05 13:41:41 +0800

    Memory Allocation in COM https://msdn.microsoft.com/en-us/library/windows/desktop/ff485847.aspx

--- COMLab/COMLab.cpp
+++ COMLab/COMLab.cpp
@@ -23,6 +23,31 @@ int APIENTRY wWinMain(
 		if (SUCCEEDED(hr))
 		{
 			// Use the FileOpenDialog object.
+
+			// Show the Open dialog box.
+			hr = pFileOpen->Show(NULL);
+
+			// Get the file name from the dialog box.
+			if (SUCCEEDED(hr))
+			{
+				IShellItem *pItem;
+				hr = pFileOpen->GetResult(&pItem);
+				if (SUCCEEDED(hr))
+				{
+					PWSTR pszFilePath;
+					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+					// Display the file name to the user.
+					if (SUCCEEDED(hr))
+					{
+						MessageBox(NULL, pszFilePath, L"File Path", MB_OK);
+						CoTaskMemFree(pszFilePath);
+					}
+
+					pItem->Release();
+				}
+			}
+
 			pFileOpen->Release();
 		}
 		else
