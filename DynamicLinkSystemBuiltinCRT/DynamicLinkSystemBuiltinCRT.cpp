98763677ccdb68408753ad8ed335b63984ae92dd
fo40225 <fo40225@hotmail.com>
2016-03-07 22:00:14 +0800

    DynamicLinkSystemBuiltinCRT https://kobyk.wordpress.com/2007/07/20/dynamically-linking-with-msvcrtdll-using-visual-c-2005/

---
+++ DynamicLinkSystemBuiltinCRT/DynamicLinkSystemBuiltinCRT.cpp
@@ -0,0 +1,11 @@
+// DynamicLinkSystemBuiltinCRT.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+
+int main()
+{
+	printf("hello, dynamic link system built-in C Run-time Library\n");
+	cout << "hello, dynamic link system built-in Standard C++ Library" << endl;
+	return 0;
+}
